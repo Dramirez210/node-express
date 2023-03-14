5a418c6cabb50c9f9e63304944c98f1e868a903b
Dramirez <funkadelick485@gmail>
2023-03-14 17:26:38 -0600

    node-express

---
+++ cursos.js
@@ -0,0 +1,60 @@
+let infoCursos = {
+    programacion: [
+        {
+            id:1,
+            titulo: "Aprende Python",
+            lenguaje: "python",
+            vistas: 15629,
+            nivel: "basico"
+        },
+        {
+            id:2,
+            titulo: "Aprende JavaScript",
+            lenguaje: "javaScript",
+            vistas: 18029,
+            nivel: "basico"
+        },
+        {
+            id:3,
+            titulo: "Aprende Java",
+            lenguaje: "java",
+            vistas: 10292,
+            nivel: "basico"
+        },
+        {
+            id:4,
+            titulo: "Java POO",
+            lenguaje: "java",
+            vistas: 12394,
+            nivel: "basico"
+        },
+    ],
+    matematicas: [
+        {
+            id:1,
+            titulo: "Algebra y geometria",
+            academia: "ciencia y tecnologia",
+            tema: "algebra",
+            vistas: 80820,
+            nivel: "basico"
+        },
+        {
+            id:2,
+            titulo: "Mecanica l",
+            academia: "ciencia y tecnologia",
+            tema: "fisica",
+            vistas: 11329,
+            nivel: "basico"
+        },
+        {
+            id:3,
+            titulo: "Calculo vectorial",
+            academia: "ciencia y tecnologia",
+            tema: "integrales",
+            vistas: 8091,
+            nivel: "intermedio"
+        },
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
